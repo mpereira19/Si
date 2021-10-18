5ba34febb845b4474163b467b80db17d8ba5d958
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2021-10-18 16:31:33 +0100

    Summary function conclusion

--- src/si/util/util.py
+++ src/si/util/util.py
@@ -1,6 +1,9 @@
 import itertools
 
 # Y is reserved to idenfify dependent variables
+import numpy as np
+import pandas as pd
+
 ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXZ'
 
 __all__ = ['label_gen', 'summary']
@@ -32,4 +35,23 @@ def summary(dataset, format='df'):
     :param format: Output format ('df':DataFrame, 'dict':dictionary ), defaults to 'df'
     :type format: str, optional
     """
-    pass
+    if dataset.hasLabel():
+        fullds = np.hstack((dataset.X, dataset.Y.reshape(len(dataset.Y))))
+        columns = dataset._xnames[:]+[dataset._yname]
+    else:
+        fullds = dataset.X
+        columns = dataset._xnames[:]
+
+    _means = np.mean(fullds, axis=0)
+    _vars = np.var(fullds, axis=0)
+    _mins = np.min(fullds, axis=0)
+    _maxs = np.max(fullds, axis=0)
+    stats = {}
+    for i in range (fullds.shape[1]):
+        stat = {'mean': _means[1], 'var': _vars[1], 'min':_mins[1], 'max':_maxs[1]}
+        stats[columns[i]] = stat
+    if format == 'df':
+        import pandas as pd
+        df = pd.DataFrame(stats)
+    else:
+        return stats
