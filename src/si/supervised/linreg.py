250126247b9b07cfec8e7728c054aaf938920312
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2021-11-26 20:43:39 +0000

    LogisticRegression and LogisticRegressionReg correction.

--- src/si/supervised/linreg.py
+++ src/si/supervised/linreg.py
@@ -1,6 +1,7 @@
 from .modelo import Modelo
 import numpy as np
 from ..util import mse, sigmoid, add_intersect
+from ..supervised import modelo
 
 __all__ = ['LinearRegression', 'LinearRegressionReg', 'LogisticRegression', 'LogisticRegressionReg']
 
@@ -111,9 +112,8 @@ class LogisticRegression:
 
 	' Regressão Logística sem regularização'
 
-	def __init__(self, gd=False, epochs=1000, lr=0.001):
+	def __init__(self, epochs=1000, lr=0.1):
 		super(LogisticRegression, self).__init__()
-		self.gd = gd
 		self.theta = None
 		self.epochs = epochs
 		self.lr = lr
@@ -137,12 +137,12 @@ class LogisticRegression:
 		for epoch in range(self.epochs):
 			z = np.dot(X, self.theta)
 			h = sigmoid(z)
-			grad = np.dot(X.T, (h - y))
+			grad = np.dot(X.T, (h - y)) / y.size
 			self.theta -= self.lr * grad
-			self.history[epoch] = [self.theta[:], self.cost()]
+			self.history[epoch] = [self.theta.copy(), self.cost()]
 
 	def predict(self, x):
-		assert self.is_fitted, 'Model must be fit before predict'
+		assert self.is_fitted, 'Model must be fit before predicting'
 		hs = np.hstack(([1], x))
 		p = sigmoid(np.dot(self.theta, hs))
 		if p >= 0.5: res = 1
@@ -153,34 +153,23 @@ class LogisticRegression:
 		X = add_intersect(X) if X is not None else self.X
 		y = y if y is not None else self.Y
 		theta = theta if theta is not None else self.theta
+		m, n = X.shape
 		h = sigmoid(np.dot(X, theta))
 		cost = (-y * np.log(h) - (1 - y) * np.log(1-h))
-		res = np.sum(cost)/X.shape[0]
+		res = np.sum(cost) / m
 		return res
 
 
-class LogisticRegressionReg:
+class LogisticRegressionReg(LogisticRegression):
 
 	' Regressão Logística com regularização'
 
-	def __init__(self, gd=False, epochs=1000, lr=0.001, lbd=1):
+	def __init__(self, epochs=1000, lr=0.1, lbd=1):
 		super(LogisticRegressionReg, self).__init__()
-		self.gd = gd
 		self.theta = None
 		self.epochs = epochs
 		self.lr = lr
-		self.lbd = lbd
-
-	def fit(self, dataset):
-		X, y = dataset.getXy()
-		X = add_intersect(X)
-		########
-		# Só é necessário para fazer o score (cost) caso não queiram dar os dados
-		self.X = X
-		self.Y = y
-		##########
-		self.train(X, y)
-		self.is_fitted = True
+		self.lbd = lbd  # lbd = lambda
 
 	def train(self, X, y):
 		m, n = X.shape
@@ -189,26 +178,19 @@ class LogisticRegressionReg:
 		for epoch in range(self.epochs):
 			z = np.dot(X, self.theta)
 			h = sigmoid(z)
-			grad = np.dot(X.T, (h - y))
-			reg = self.lbd / m * self.theta
-			self.theta -= self.lr * (grad - reg)
+			grad = np.dot(X.T, (h - y)) / y.size
+			reg = (self.lbd / m) * self.theta[1:]  ###### parentesis
+			grad[1:] = grad[1:] + reg
+			self.theta -= self.lr * grad
 			self.history[epoch] = [self.theta[:], self.cost()]
-
-	def predict(self, x):
-		assert self.is_fitted, 'Model must be fit before predict'
-		hs = np.hstack(([1], x))
-		p = sigmoid(np.dot(self.theta, hs))
-		if p >= 0.5:
-			res = 1
-		else:
-			res = 0
-		return res
 
 	def cost(self, X=None, y=None, theta=None):
 		X = add_intersect(X) if X is not None else self.X
 		y = y if y is not None else self.Y
 		theta = theta if theta is not None else self.theta
+		m = X.shape[0]
 		h = sigmoid(np.dot(X, theta))
 		cost = (-y * np.log(h) - (1 - y) * np.log(1 - h))
-		res = np.sum(cost) / X.shape[0]
-		return res
+		reg = np.dot(theta[1:], theta[1:]) * self.lbd / (2 * m)
+		res = np.sum(cost) / m
+		return res + reg
