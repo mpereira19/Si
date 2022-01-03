55eb607fe053654c3b81def6c9f46e8c10d8f1e2
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2022-01-03 16:36:02 +0000

    Class 3.01.22

--- src/si/util/util.py
+++ src/si/util/util.py
@@ -27,6 +27,25 @@ def label_gen(n):
     return [gen() for _ in range(n)]
 
 
+def l1_distance(x, y):
+    """Computes the manhatan distance of a point (x) to a set of
+    points y.
+    x.shape=(n,) and y.shape=(m,n)
+    """
+    import numpy as np
+    dist = (np.absolute(x - y)).sum(axis=1)
+    return dist
+
+
+def l2_distance(x, y):
+    """Computes the euclidean distance of a point (x) to a set of
+    points y.
+    x.shape=(n,) and y.shape=(m,n)
+    """
+    dist = ((x - y) ** 2).sum(axis=1)
+    return dist
+
+
 def euclidian_distance(x, y):
     dist = np.sqrt(((x -y) ** 2).sum(axis=1))
     return dist
@@ -55,3 +74,33 @@ def sigmoid(z):
 def add_intersect(X):
     return np.hstack((np.ones((X.shape[0], 1)), X))
 
+
+def to_categorical(y, num_classes=None, dtype='float32'):
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
+def minibatch(X, batchsize=256, shuffle=True):
+    N = X.shape[0]
+    ix = np.arange(N)
+    n_batches = int(np.ceil(N / batchsize))
+
+    if shuffle:
+        np.random.shuffle(ix)
+
+    def mb_generator():
+        for i in range(n_batches):
+            yield ix[i * batchsize: (i + 1) * batchsize]
+
+    return mb_generator(),
